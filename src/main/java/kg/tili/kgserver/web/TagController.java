9ea7244466c7d323ebe776a8f8f7450393eb0253
v.markitanov <v.markitanov@outlook.com>
2023-04-21 08:34:34 +0300

    Tag controller.

---
+++ src/main/java/kg/tili/kgserver/web/TagController.java
@@ -0,0 +1,48 @@
+package kg.tili.kgserver.web;
+
+import kg.tili.kgserver.dto.ResponseDto;
+import kg.tili.kgserver.dto.TagDto;
+import kg.tili.kgserver.entity.Tag;
+import kg.tili.kgserver.repository.TagRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Markitanov Vadim
+ * @since 21.04.2023
+ */
+@RestController
+@AllArgsConstructor
+public class TagController {
+    private final TagRepo tagRepo;
+
+    @RequestMapping(value = "/get_tags", method = RequestMethod.GET)
+    public ResponseEntity<ResponseDto<List<TagDto>>> getAllTags() {
+        List<TagDto> result = new ArrayList<>();
+
+        for (Tag tag : tagRepo.findAll()) {
+            result.add(new TagDto(tag.getValue(), tag.getLabel()));
+        }
+
+        return ResponseEntity.ok(ResponseDto.<List<TagDto>>success().data(result).build());
+    }
+
+    @RequestMapping(value = "/save_tag", method = RequestMethod.POST)
+    public ResponseEntity<ResponseDto<List<TagDto>>> saveTag(@RequestBody TagDto dto) {
+        System.out.println("New tag: " + dto);
+        Tag tag = new Tag();
+//        if (dto.value != null) {
+//            tag.setValue(dto.value);
+//        }
+        tag.setLabel(dto.label);
+        tagRepo.save(tag);
+        return getAllTags();
+    }
+}
